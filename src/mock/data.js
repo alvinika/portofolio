fd78561dc707b201c244f77c3f95ce3e48c4bf9d
alvinika <alfin1715@gmail.com>
2021-01-04 20:17:50 +0700

    change color

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,7 +11,7 @@ export const headData = {
 export const heroData = {
   title: 'Hello, my name is',
   name: 'Alvin Ika',
-  subtitle: 'Junior Programmer',
+  subtitle: 'Im a Programmer',
   cta: '',
 };
 
